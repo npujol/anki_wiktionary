fef13019893b19ace3f721e7a0f4a23331740eb4
npujol <naivy.luna@gmail.com>
2024-05-19 11:50:25 +0200

    Change telegram bot structure

--- app/telegram_bot/telegram_bot.py
+++ app/telegram_bot/telegram_bot.py
@@ -1,224 +1,57 @@
 import logging
-from pathlib import Path
-from telegram import Update, BotCommand, Message
+
+from environs import Env
+from telegram import BotCommand, Update
 from telegram.ext import (
     Application,
     CommandHandler,
-    ContextTypes,
+    MessageHandler,
     filters,
-    CallbackContext,
 )
 
-from environs import Env
-from telegram.ext._handlers.messagehandler import MessageHandler
-
-
-from app.main import (
-    generate_audio,
-    get_anki_note_data,
-    save_anki_note_to_list,
-    send_card_using_anki_web,
+from app.telegram_bot.handlers import (
+    handle_audio,
+    handle_help,
+    handle_web_word,
+    handle_word,
+    message_handle,
+    unsupport_message_handle,
 )
 
-
+# Set up logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
+# Load environment variables
 env = Env()
 env.read_env()
-
-# Telegram bot token
 TOKEN = env("TELEGRAM_TOKEN")
-
-# File path to store Anki notes
 ANKI_NOTES_FILE_PATH = env("ANKI_NOTES_FILE_PATH") or "anki_notes.txt"
 
 
-logger = logging.getLogger(__name__)
-
-
-async def handle_word(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """
-    Asynchronously handles a word update.
-
-    Args:
-        update (Update): The update object.
-        context (ContextTypes.DEFAULT_TYPE): The context object.
-
-    Returns:
-        None
-    """
-    args = context.args or []
-    word = " ".join(args)
-    message = update.message
-    if not message:
-        logger.error("No message provided.")
-        return
-    if word:
-        await _handle_word(word, message)
-    else:
-        msg = "Provide the word to create the anki note:"
-        logger.info(msg)
-        await message.reply_text(msg)
-        word = " ".join(args)
-        message = update.message
-    if word:
-        await _handle_word(word, message)
-
-
-async def _handle_word(word: str, message: Message):
-    if word:
-        await save_anki_note_to_list(word)
-        msg = f"Anki note of {word=} saved successfully."
-        logger.info(msg)
-        await message.reply_text(msg)
-        note = await get_anki_note_data(word)
-        if not note:
-            await message.reply_text("Anki note could not be created.")
-            return
-        await message.reply_text(note.pretty_print())
-        if note is not None and note.audio and note.audio[0]:
-            filepath = Path(__file__).parent.parent / f"files/{word}.mp3"
-            await message.reply_audio(filepath)
-            filepath.unlink()
-
-
-async def handle_audio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    args = context.args or []
-    text = " ".join(args)
-    message = update.message
-    if not message:
-        logger.error("No message provided.")
-        return
-    if text:
-        filepath = await generate_audio(text)
-        await message.reply_audio(filepath)
-        filepath.unlink()
-    else:
-        msg = "Please provide a sentence to create audio."
-        logger.info(msg)
-        await message.reply_text(msg)
-
-
-async def handle_help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """
-    Asynchronously handles a word update.
-
-    Args:
-        update (Update): The update object.
-        context (ContextTypes.DEFAULT_TYPE): The context object.
-
-    Returns:
-        None
-    """
-    message = update.message
-    if not message:
-        logger.error("No message provided.")
-        return
-    msg = (
-        "Commands:\n"
-        "/help: Provides help of the bot.\n/word: Creates an Anki note content and"
-        "set the Note to the queue to save in Anki.\n/audio: Creates audio from a text"
-        "\n/web_word: create deck and send it to web browser"
-    )
-    logger.info(msg)
-    await message.reply_text(msg)
-
-
-async def handle_web_word(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    args = context.args or []
-    word = " ".join(args)
-    message = update.message
-    if not message:
-        logger.error("No message provided.")
-        return
-    if not word:
-        msg = "Please provide a word to create an Anki note."
-        logger.info(msg)
-        await message.reply_text(msg)
-
-    note = await send_card_using_anki_web(word)
-    if note is None:
-        msg = "Anki note could not be created."
-        logger.error(msg)
-        await message.reply_text(msg)
-        return
-
-    logger.info("Anki note created successfully.")
-    await message.reply_text(note.pretty_print())
-    if note is not None and note.audio and note.audio[0]:
-        filepath = Path(__file__).parent.parent / f"files/{word}.mp3"
-        await message.reply_audio(filepath)
-        filepath.unlink()
-
-
-async def handle_text_without_command(
-    update: Update, context: ContextTypes.DEFAULT_TYPE
-) -> None:
-    """
-    Asynchronously handles a text update without a command.
-
-    Args:
-        update (Update): The update object.
-        context (ContextTypes.DEFAULT_TYPE): The context object.
-
-    Returns:
-        None
-    """
-    word = update.message.text
-    if word:
-        await handle_word(update, context)
-
-
-async def unsupport_message_handle(
-    update: Update, context: CallbackContext, message=None
-):
-    error_text = "I don't know how to process other inputs. I only work with text."
-    logger.error(error_text)
-    await update.message.reply_text(error_text)
-    return
-
-
-async def message_handle(
-    update: Update, context: CallbackContext, message=None, use_new_dialog_timeout=True
-):
-    _message = message or update.message.text
-
-    if not _message:
-        await unsupport_message_handle(update, context, message)
-        return
-    word = _message
-
-    await update.message.reply_text("We are using the default command /web_word")
-    note = await send_card_using_anki_web(word)
-    if note is None:
-        msg = "Anki note could not be created."
-        logger.error(msg)
-        await message.reply_text(msg)
-        return
-
-    logger.info("Anki note created successfully.")
-    await message.reply_text(note.pretty_print())
-    if note is not None and note.audio and note.audio[0]:
-        filepath = Path(__file__).parent.parent / f"files/{word}.mp3"
-        await message.reply_audio(filepath)
-        filepath.unlink()
-
-
 async def post_init(application: Application):
-    # Set bot commands, these commands are included in the menu button.
+    """
+    Set bot commands after initializing the bot.
+
+    Args:
+        application (Application): The application object.
+    """
     await application.bot.set_my_commands(
         [
             BotCommand("/word", "Create an Anki note for a word"),
             BotCommand("/help", "Show help message"),
+            BotCommand("/audio", "Create audio from text"),
+            BotCommand("/web_word", "Create an Anki note and send it to AnkiWeb"),
         ]
     )
 
 
 def main() -> None:
-    """Run bot."""
-    # Create the Application and pass it your bot's token.
+    """
+    Run the bot.
+    """
     application = Application.builder().token(TOKEN).build()
 
     # Register command handlers
@@ -226,14 +59,15 @@ def main() -> None:
     application.add_handler(CommandHandler("help", handle_help))
     application.add_handler(CommandHandler("audio", handle_audio))
     application.add_handler(CommandHandler("web_word", handle_web_word))
-    # Add command handlers for creating Anki note without command
+
+    # Register message handlers
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, message_handle)
     )
-
     application.add_handler(
         MessageHandler(
-            filters.VIDEO & ~filters.COMMAND & filters.PHOTO, unsupport_message_handle
+            filters.VIDEO & ~filters.COMMAND & filters.PHOTO,
+            unsupport_message_handle,
         )
     )
 
