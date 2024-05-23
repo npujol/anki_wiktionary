919e6ebc1751993b84d9c0464f084c6d2cf94ffb
leiserfg <leiserfg@gmail.com>
2024-05-23 23:18:45 +0200

    Make build build

--- flake.nix
+++ flake.nix
@@ -2,44 +2,70 @@
   description = "Telegram bot to generate Anki Cards";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix}:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; }) mkPoetryApplication;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    poetry2nix,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        inherit (poetry2nix.lib.mkPoetry2Nix {inherit pkgs;}) mkPoetryApplication defaultPoetryOverrides;
+      in
+        with pkgs; rec {
+          # TODO Check why is compiling chromium
+          # Development environment
+          devShell = mkShell {
+            name = "anki_wiktionary";
+            buildInputs = [
+              pkgs.python311
+              pkgs.poetry
+            ];
+            shellHook = ''
+              poetry env use ${pkgs.lib.getExe pkgs.python311}
+              export VIRTUAL_ENV=$(poetry env info --path)
+              export PATH=$VIRTUAL_ENV/bin/:$PATH
+              export CHROMEDRIVER_PATH=${pkgs.lib.getExe pkgs.chromedriver}
+              export BROWSER_PATH=${pkgs.lib.getExe pkgs.brave}
+            '';
+          };
 
-      in with pkgs; rec {
-        # TODO Check why is compiling chromium
-        # Development environment
-        devShell = mkShell {
-          name = "anki_wiktionary";
-          buildInputs = [
-            pkgs.python311
-            pkgs.poetry
-          ];
-          shellHook = ''
-            poetry env use ${pkgs.lib.getExe pkgs.python311}
-            export VIRTUAL_ENV=$(poetry env info --path)
-            export PATH=$VIRTUAL_ENV/bin/:$PATH
-            export CHROMEDRIVER_PATH=${pkgs.lib.getExe pkgs.chromedriver}
-            export BROWSER_PATH=${pkgs.lib.getExe pkgs.brave}
-          '';
-        };
+          # Runtime package
+          packages.app = mkPoetryApplication {
+            projectDir = ./.;
+            preferWheels = true;
 
-        # Runtime package
-        packages.app = mkPoetryApplication {
-          projectDir = ./.;
-        };
+            #  without  preferWheels something like this needs to be done
+            # overrides =
+            #   defaultPoetryOverrides.extend
+            #   (final: prev: {
+            #     pyvirtualdisplay =
+            #       prev.pyvirtualdisplay.overridePythonAttrs
+            #       (
+            #         old: {
+            #           buildInputs = (old.buildInputs or []) ++ [prev.setuptools];
+            #         }
+            #       );
+            #
+            #     pytest-insta =
+            #       prev.pytest-insta .overridePythonAttrs
+            #       (
+            #         old: {
+            #           buildInputs = (old.buildInputs or []) ++ [prev.poetry];
+            #         }
+            #       );
+            #   });
+          };
 
-        # The default package when a specific package name isn't specified.
-        defaultPackage = packages.app;
-        # formatter.${system} = pkgs.alejandra;
-        formatter.x86_64-linux = pkgs.alejandra;
-
-      }
+          # The default package when a specific package name isn't specified.
+          defaultPackage = packages.app;
+          formatter = pkgs.alejandra;
+        }
     );
 }
