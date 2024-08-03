442b3838c6294c6365dc4710f9773f6b056e7f68
npujol <naivy.luna@gmail.com>
2024-08-03 15:59:12 +0200

    Clean test from main

--- app/data_processors/tests/test_data_processor.py
+++ app/data_processors/tests/test_data_processor.py
@@ -12,3 +12,28 @@ def test_get_anki_note(snapshot: Any) -> None:
     assert snapshot("json") == result.model_dump(
         mode="python", by_alias=True, exclude_none=True
     ), "The result does not match the snapshot"
+
+
+@pytest.mark.vcr(mode="once")
+@pytest.mark.parametrize(
+    "processor_name",
+    [
+        "verben",
+        "wiktionary",
+        "ollama",
+    ],
+)
+def test_send_card_using_anki_web(
+    processor_name: str,
+    snapshot: Any,
+) -> None:
+    note = NoteDataProcessor(
+        deck_name="deck_name",
+        model_name="model_name",
+    ).get_anki_note(
+        word="Abend",
+        processor_name=processor_name,
+    )
+
+    assert note, "Add note failed"
+    assert snapshot("json") == note.model_dump(mode="python", by_alias=True)
--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -3,47 +3,21 @@ from typing import Any
 
 import pytest
 
-from app.data_processors import NoteDataProcessor
 from app.main import generate_note, get_anki_note_data
 
 
 @pytest.mark.vcr()
-def test_generate_notes(snapshot: Any):
+def test_generate_notes_is_successful(snapshot: Any) -> None:
     result = generate_note(word="Abend")
     assert result, "Add note failed"
-    assert snapshot("json") == result
+    assert snapshot("json") == result, "The result does not match the snapshot"
 
 
 @pytest.mark.vcr()
 @pytest.mark.asyncio
-async def test_get_anki_note_data(
+async def test_get_anki_note_data_is_successful(
     snapshot: Any,
-):
+) -> None:
     result = await asyncio.create_task(get_anki_note_data(word="Abend"))
     assert result, "Add note failed"
     assert snapshot("json") == result.model_dump(mode="python", by_alias=True)
-
-
-@pytest.mark.vcr(mode="once")
-@pytest.mark.parametrize(
-    "processor_name",
-    [
-        "verben",
-        "wiktionary",
-        "ollama",
-    ],
-)
-def test_send_card_using_anki_web(
-    processor_name: str,
-    snapshot: Any,
-):
-    note = NoteDataProcessor(
-        deck_name="deck_name",
-        model_name="model_name",
-    ).get_anki_note(
-        word="Abend",
-        processor_name=processor_name,
-    )
-
-    assert note, "Add note failed"
-    assert snapshot("json") == note.model_dump(mode="python", by_alias=True)
