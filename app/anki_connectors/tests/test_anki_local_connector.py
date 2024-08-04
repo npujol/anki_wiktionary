541ec9ad79c5420edf14de8ac0d6b538e55cb64f
npujol <naivy.luna@gmail.com>
2024-08-04 11:09:10 +0200

    Add tests of local connector

--- app/anki_connectors/tests/test_anki_local_connector.py
+++ app/anki_connectors/tests/test_anki_local_connector.py
@@ -3,6 +3,7 @@ from typing import Any
 import pytest
 
 from app.anki_connectors.anki_local_connector import AnkiLocalConnector
+from app.anki_connectors.errors import CollectionNotFoundError
 from app.serializers import CustomNote, Note
 
 
@@ -10,21 +11,53 @@ from app.serializers import CustomNote, Note
 def test_add_note(
     anki_connector: AnkiLocalConnector,
     note_obj: Note,
+    snapshot: Any,
 ) -> None:
-    result = anki_connector.add_note(note=note_obj)
+    result: dict[str, Any] | None = anki_connector.add_note(note=note_obj)
+
     assert result, "Add note failed"
+    assert snapshot("json") == result, "The returned JSON does not match the snapshot"
 
 
 @pytest.mark.vcr()
 def test_add_note_allow_duplicated(
     anki_connector: AnkiLocalConnector,
     note_obj: Note,
+    snapshot: Any,
 ) -> None:
-    result = anki_connector.add_note(note=note_obj)
+    result: dict[str, Any] | None = anki_connector.add_note(note=note_obj)
+
     assert result, "Add note failed"
 
     result = anki_connector.add_note(note=note_obj)
-    assert result
+
+    assert result, "Add note failed"
+    assert snapshot("json") == result, "The returned JSON does not match the snapshot"
+
+
+@pytest.mark.vcr()
+def test_add_custom_note(
+    anki_connector: AnkiLocalConnector,
+    custom_note_obj: CustomNote,
+    snapshot: Any,
+) -> None:
+    result: dict[str, Any] | None = anki_connector.add_note(note=custom_note_obj)
+
+    assert result, "Add note failed"
+    assert snapshot("json") == result, "The returned JSON does not match the snapshot"
+
+
+@pytest.mark.vcr()
+def test_add_custom_note_with_failed_connection(
+    anki_connector: AnkiLocalConnector,
+    custom_note_obj: CustomNote,
+    snapshot: Any,
+    caplog: Any,
+) -> None:
+    with pytest.raises(expected_exception=CollectionNotFoundError):
+        anki_connector.add_note(note=custom_note_obj)
+
+    assert snapshot("json") == caplog.records[0].message
 
 
 @pytest.mark.vcr()
@@ -32,8 +65,10 @@ def test_get_available_decks(
     anki_connector: AnkiLocalConnector,
     snapshot: Any,
 ) -> None:
-    result = anki_connector.get_available_decks()
-    assert snapshot("json") == result
+    result: dict[str, Any] | None = anki_connector.get_available_decks()
+
+    assert result, "Get available decks failed"
+    assert snapshot("json") == result, "The returned JSON does not match the snapshot"
 
 
 @pytest.mark.vcr()
@@ -50,17 +85,13 @@ def test_get_cards_from_deck(
     anki_connector: AnkiLocalConnector,
     snapshot: Any,
 ) -> None:
-    result = anki_connector.get_cards_from_deck(deck_name=deck_name)
-    assert snapshot(f"{deck_name}.json") == result
+    result: dict[str, Any] | None = anki_connector.get_cards_from_deck(
+        deck_name=deck_name
+    )
 
-
-@pytest.mark.vcr()
-def test_add_custom_note(
-    anki_connector: AnkiLocalConnector,
-    custom_note_obj: CustomNote,
-) -> None:
-    result = anki_connector.add_note(note=custom_note_obj)
-    assert result, "Add note failed"
+    assert (
+        snapshot(f"{deck_name}.json") == result
+    ), f"The returned JSON from {deck_name=} does not match the snapshot"
 
 
 @pytest.mark.vcr()
@@ -68,5 +99,7 @@ def test_get_models_and_ids(
     anki_connector: AnkiLocalConnector,
     snapshot: Any,
 ) -> None:
-    result = anki_connector.get_models_and_ids()
+    result: dict[str, Any] | None = anki_connector.get_models_and_ids()
+
+    assert result, "Get models and ids failed"
     assert snapshot("json") == result
