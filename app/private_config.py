1c3663a5b86876d743d8678b67f9e127e0ff7224
npujol <naivy.luna@gmail.com>
2024-08-04 12:01:23 +0200

    Add typing in private config

--- app/private_config.py
+++ app/private_config.py
@@ -6,21 +6,21 @@ env = Env()
 env.read_env(override=True)
 
 # Path to Browser binary to be used for selenium webdriver
-browser_binary_location: str = env("BROWSER_PATH")
-browser_driver_binary: str = env("BROWSERDRIVER_PATH") or "/usr/bin/webdriver"
+browser_binary_location: str = env("BROWSER_PATH")  # type: ignore
+browser_driver_binary: str = env("BROWSERDRIVER_PATH") or "/usr/bin/webdriver"  # type: ignore
 
-files_path: str = env("FILES_PATH", default="files")
-working_path: Path = Path(files_path)
+files_path: str = env("FILES_PATH", default="files")  # type: ignore
+working_path: Path = Path(files_path)  # type: ignore
 working_path.mkdir(parents=True, exist_ok=True)
 
 # Anki credentials and settings
-anki_note_file_path = env("ANKI_NOTES_FILE_PATH", default="anki_notes.txt")
-anki_username = env.str("ANKI_USER", default=None)
-anki_password = env.str("ANKI_PASS", default=None)
-anki_deck_name = env.str("ANKI_DECK_NAME", default="Mein Deutsch")
+anki_note_file_path: str = env("ANKI_NOTES_FILE_PATH", default="anki_notes.txt")  # type: ignore
+anki_username: str = env.str("ANKI_USER", default=None)  # type: ignore
+anki_password: str = env.str("ANKI_PASS", default=None)  # type: ignore
+anki_deck_name: str = env.str("ANKI_DECK_NAME", default="Mein Deutsch")  # type: ignore
 
 # Telegram bot credentials
-bot_token = env.str("TELEGRAM_TOKEN", default=None)
+bot_token: str = env.str("TELEGRAM_TOKEN", default=None)  # type: ignore
 
 # Ollama server url
-ollama_server_url = env.str("OLLAMA_URL", default="http://127.0.0.1:11434")
+ollama_server_url: str = env.str("OLLAMA_URL", default="http://127.0.0.1:11434")  # type: ignore
