8ff092737d42a13f0e19d20d04fa8e62cbbfefe2
npujol <naivy.luna@gmail.com>
2024-09-08 23:02:39 +0200

    Add verben parser

--- app/data_processors/processors/verben_data_processor.py
+++ app/data_processors/processors/verben_data_processor.py
@@ -2,11 +2,11 @@ import logging
 from typing import Any
 
 import requests
-from bs4 import BeautifulSoup, Tag
+from bs4 import BeautifulSoup
 
 from app.data_processors.processors.base_data_processor import BaseDataProcessor
-from app.html_processors import prune_html_tags
-from app.serializers import BasicFields, CustomNote
+from app.parsers.verben_parser import VerbenParser
+from app.serializers import CustomFields, CustomNote
 
 logger: logging.Logger = logging.getLogger(name=__name__)
 
@@ -15,7 +15,7 @@ logger: logging.Logger = logging.getLogger(name=__name__)
 class VerbenDataProcessor(BaseDataProcessor):
     def __init__(self) -> None:
         self.base_url = "https://www.verben.de/?w="
-        self.fields_class = BasicFields
+        self.fields_class = CustomFields
 
     def get_note_data(self, word: str, note: CustomNote) -> CustomNote | None:
         response: requests.Response = requests.get(
@@ -28,40 +28,36 @@ class VerbenDataProcessor(BaseDataProcessor):
         # Get the content of the response
         page_content: bytes = response.content
 
-        soup = BeautifulSoup(
+        content = BeautifulSoup(
             markup=page_content,
             features="html.parser",
         )
 
-        info_selector = "body > article > div:nth-child(1) > div.rAbschnitt"
-        lateral_info_selector = "body > article > div:nth-child(1) > div.rInfo"
+        if not content:
+            logger.error(msg=f"Could not fetch data for word '{word}' using Duden.")
+            return note
 
-        # Find the elements using the CSS selectors
-        info_element: Tag | None = soup.select_one(selector=info_selector)
-        lateral_info_element: Tag | None = soup.select_one(
-            selector=lateral_info_selector
+        content_dict: dict[str, Any] = self._extract_from_content(
+            word=word, content=content
         )
-
-        body: str = (
-            str(object=prune_html_tags(html=info_element))
-            if info_element
-            else "" + "<br>" + str(prune_html_tags(html=lateral_info_element))
-            if lateral_info_element
-            else ""
-        )
-        content: dict[str, str] = {
-            "Front": word,
-            "Back": str(body),
-        }
         updated_note: CustomNote | None = note.import_from_content(
-            content=content, fields_class=self.fields_class
+            content=content_dict, fields_class=self.fields_class
         )
 
         return updated_note
 
     def _extract_from_content(self, word: str, content: Any) -> dict[str, Any]:
-        raise NotImplementedError
+        parser = VerbenParser(soup=content)
+        return {
+            "full_word": word,
+            "plural": parser.plural,
+            "characteristics": parser.characteristics,
+            "ipa": parser.ipa,
+            "meaning": parser.meaning,
+            "example1": parser.example1,
+            "example2": parser.example2,
+        }
 
-    def is_content_complete(self, content: dict[str, Any]) -> bool:
-        # TODO: Review this and move to a base class
-        return False
+    # def is_content_complete(self, content: dict[str, Any]) -> bool:
+    #     # TODO: Review this and move to a base class
+    #     return False
