3372be05bf00643e7f0afef9a238a602365dda59
npujol <naivy.luna@gmail.com>
2025-02-11 21:26:34 +0100

    Add gemini processor

--- app/data_processors/processors/__init__.py
+++ app/data_processors/processors/__init__.py
@@ -1,5 +1,6 @@
 from .base_data_processor import BaseDataProcessor  # type: ignore
 from .duden_data_processor import DudenDataProcessor  # type: ignore
+from .gemini_data_processor import GeminiDataProcessor  # type: ignore
 from .ollama_data_processor import OllamaDataProcessor  # type: ignore
 from .verben_data_processor import VerbenDataProcessor  # type: ignore
 from .wiktionary_data_processor import WiktionaryDataProcessor  # type: ignore
@@ -10,4 +11,5 @@ __all__ = [
     "OllamaDataProcessor",
     "VerbenDataProcessor",
     "WiktionaryDataProcessor",
+    "GeminiDataProcessor",
 ]
